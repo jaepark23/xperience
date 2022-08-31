d751273e48a88981dd3424ccf2bf65fb1cae5776
Jae Park <85603334+jaepark23@users.noreply.github.com>
2022-08-31 00:22:18 -0500

    Update Projects.tsx

--- Projects.tsx
+++ Projects.tsx
@@ -10,9 +10,11 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 
+import "./styles.scss"
 
-import { Navigation } from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper";
 
 const Project1 = lazy(() => import("../projects/Project1"))
 
@@ -21,12 +23,21 @@ function Projects(props: {}) {
     const theme = useTheme()
     const navigate = useNavigate()
 
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index: number, className: string) {
+          return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+      };
+
     useEffect(() => {
         theme.setLoadState(-2)
     }, [])
 
     return (
-        <Swiper pagination={true} navigation={true} modules={[Navigation]} className="mySwiper">
+        <Swiper pagination={pagination} autoplay = {{delay: 8000, disableOnInteraction: false }} slidesPerView = {1} navigation={true} modules={[Navigation, Pagination, Autoplay]} className="mySwiper">
+            <SwiperSlide> <Project1/> </SwiperSlide>
+            <SwiperSlide> <Project1/> </SwiperSlide>
             <SwiperSlide> <Project1/> </SwiperSlide>
             <SwiperSlide> <Project1/> </SwiperSlide>
         </Swiper>
