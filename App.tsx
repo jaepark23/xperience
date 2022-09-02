5b716add16ea53402eafe4e2ba771fda754ff77f
Jae Park <85603334+jaepark23@users.noreply.github.com>
2022-09-02 09:12:55 -0500

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React, {lazy} from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import ThemeProvider from "./components/generic/ThemeContext";
+import ScreenProvider from "./components/generic/ScreenContext";
+import AuthProvider from "./components/generic/AuthContext";
+
+const Home = lazy(() => import("./components/pages/Home"))
+const Layout = lazy(() => import("./components/layout/Layout"))
+const Coding = lazy(() => import("./components/pages/Coding"))
+const Consulting = lazy(() => import("./components/pages/Consulting"))
+const Projects = lazy(() => import("./components/pages/Projects"))
+const Users = lazy(() => import("./components/pages/Users"))
+
+function App() {
+      return (
+          <Router>
+              <ThemeProvider>
+                  <ScreenProvider>
+                      <AuthProvider>
+                          <Layout>
+                              <Routes>
+                                  <Route path={"*"} element={<Home />} />
+                                  <Route path={"/coding"} element={<Coding />} />
+                                  <Route path={"/consulting"} element={<Consulting />} />
+                                  <Route path={"/projects"} element={<Projects />} />
+                                  <Route path={"/users"} element={<Users />} />
+                              </Routes>
+                          </Layout>
+                      </AuthProvider>
+                  </ScreenProvider>
+              </ThemeProvider>
+          </Router>
+      );
+}
+
+export default App;
