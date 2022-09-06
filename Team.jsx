8a42c2ec64eaca636a6ebb62fd15f29f70a45002
Jae Park <85603334+jaepark23@users.noreply.github.com>
2022-09-06 18:45:29 -0500

    Update Team.jsx

--- Team.jsx
+++ Team.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useRef, useState, lazy} from "react";
+import React, { useEffect, useRef, useState, lazy } from "react";
 import "../../scss/core.scss"
 import "../../scss/pages.scss"
-import {AnimatePresence, motion} from "framer-motion";
-import {Anim} from "../../Animation";
-import {useTheme} from "../generic/ThemeContext";
-import {useNavigate} from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+import { Anim } from "../../Animation";
+import { useTheme } from "../generic/ThemeContext";
+import { useNavigate } from "react-router-dom";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -13,6 +13,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import { Navigation, Pagination, Autoplay } from "swiper";
+import GridLayout from "react-grid-layout";
 
 import "../../img/image1.png"
 import "../../img/elon.webp"
@@ -24,24 +25,31 @@ function Team(props: {}) {
     const theme = useTheme()
     const navigate = useNavigate()
 
-
+    const layout = [
+        { i: "a", x: 0, y: 0, w: 1, h: 1 },
+        { i: "b", x: 1, y: 0, w: 1, h: 1 },
+        { i: "c", x: 2, y: 0, w: 1, h: 1 },
+        { i: "d", x: 0, y: 1, w: 1, h: 1 },
+        { i: "e", x: 1, y: 1, w: 1, h: 1 },
+        { i: "f", x: 2, y: 1, w: 1, h: 1 },
+    ];
     useEffect(() => {
         theme.setLoadState(-2)
     }, [])
 
     return <div className="apex-users w-100 h-100 col-cc">
         <AnimatePresence>
-        { theme.loadState === -2 &&
-            <motion.div className="document col-st" variants={Anim.bounceY(1200).spring(240, 0, 30).build()}
-                        initial="inactive" animate="active" exit="inactive"
-            >
-                <div className="h0 oxanium bold col-cc" style={{
-                    backgroundImage: `url(${Background})`,
-                    backgroundSize: "100% auto",
-                    backgroundPosition: "50% 50%",
-                }}>
-                    Meet Our Team
-                    {/* <motion.div className="back" tabIndex={0} onClick={() => {
+            {theme.loadState === -2 &&
+                <motion.div className="document col-st" variants={Anim.bounceY(1200).spring(240, 0, 30).build()}
+                    initial="inactive" animate="active" exit="inactive"
+                >
+                    <div className="h0 oxanium bold col-cc" style={{
+                        backgroundImage: `url(${Background})`,
+                        backgroundSize: "100% auto",
+                        backgroundPosition: "50% 50%",
+                    }}>
+                        Meet Our Team
+                        {/* <motion.div className="back" tabIndex={0} onClick={() => {
                         theme.setLoadState(-1);
                         setTimeout(() => {
                             navigate("/");
@@ -49,9 +57,44 @@ function Team(props: {}) {
                     }} whileHover={{boxShadow: "0 0 1rem white"}}>
                         {"<"}
                     </motion.div> */}
-                </div>
-
-                <div className="list row-bc">
+                    </div>
+                    <GridLayout
+                        className="layout"
+                        layout={layout}
+                        cols={3}
+                        rowHeight={360}
+                        width={1335}
+                        containerPadding = {[20, -10]}
+                        margin = {[20, 20]}
+                    >
+                        <div key="a">
+                            <img src = {require("../../img/bill.jpg")} />
+                            <figcaption className = "name oxanium bold"> Bill Gates </figcaption>
+                            <figcaption className = "caption oxanium"> Co-founder of Microsot, super rich. I need two lines right now. </figcaption>
+                        </div>
+                        <div key="b">
+                            <img src = {require("../../img/lueken.jpg")} />
+                            <figcaption className = "name oxanium bold"> Mr. Lueken </figcaption>
+                            <figcaption className = "caption oxanium"> Award winning Mr. Lueken. Fluent at everything. </figcaption>
+                        </div>
+                        <div key="c">
+                            <img src = {require("../../img/tim.jpg")} />
+                            <figcaption className = "name oxanium bold"> Tim Cook </figcaption>
+                            <figcaption className = "caption oxanium"> Co-founder at Apple, good at making phone related stuff</figcaption>
+                        </div>
+                        <div key="d">
+                            <img src = {require("../../img/mark.jpg")} />
+                            <figcaption className = "name oxanium bold"> Mark Zuckerberg </figcaption>
+                            <figcaption className = "caption oxanium"> CEO of Facebook or META, lizard man lizard man </figcaption>
+                        </div>
+                        <div key="e">
+                            <img src = {require("../../img/elon.jpg")} />
+                            <figcaption className = "name oxanium bold"> Elon Musk </figcaption>
+                            <figcaption className = "caption oxanium"> CEO of Tesla and SpaceX. Fluent in everything. </figcaption>
+                        </div>
+                    </GridLayout>
+                    
+                    {/* <div className="list row-bc">
                     <div className= "img-with-text">
                         <img src = {require("../../img/bill.jpg")} />
                         <figcaption className = "name oxanium bold"> Bill Gates </figcaption>
@@ -89,12 +132,12 @@ function Team(props: {}) {
                         <figcaption className = "name oxanium bold"> Jeff Bezos </figcaption>
                         <figcaption className = "oxanium"> CEO of Amazon @ amazon.com likes money</figcaption>
                     </div>
-                </div>
-                <motion.div tabIndex={0} onClick={() => window.open("mailto:drshika.asher@vhhscougars.org")} className="click-to col-cc oxanium h6 bold w-100 text-centered" whileHover={{boxShadow: "0 0 1rem white"}}>
-                    Click to contact Drshika Asher (drshika.asher@vhhscougars.org) for more information.
+                </div> */}
+                    <motion.div tabIndex={0} onClick={() => window.open("mailto:drshika.asher@vhhscougars.org")} className="click-to col-cc oxanium h6 bold w-100 text-centered" whileHover={{ boxShadow: "0 0 1rem white" }}>
+                        Click to contact Drshika Asher (drshika.asher@vhhscougars.org) for more information.
+                    </motion.div>
                 </motion.div>
-            </motion.div>
-        }
+            }
         </AnimatePresence>
     </div>
 }
