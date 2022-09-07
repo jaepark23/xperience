3c4502835d8ec88d7b0c4242a3398a875d6eb4dc
Jae Park <85603334+jaepark23@users.noreply.github.com>
2022-09-06 19:45:32 -0500

    Update Projects.tsx

--- Projects.tsx
+++ Projects.tsx
@@ -36,7 +36,7 @@ function Projects(props: {}) {
 
     return (
         // 1100x1200 resolution for images 
-        <Swiper pagination={pagination} loop={true} autoplay = {{delay: 8000, disableOnInteraction: false }} slidesPerView = {1} navigation={true} modules={[Navigation,  Autoplay]} className="mySwiper">
+      <Swiper pagination={pagination} loop={true} autoplay = {{delay: 8000, disableOnInteraction: false }} slidesPerView = {1} navigation={true} modules={[Navigation,  Autoplay]} className="mySwiper">
             <SwiperSlide> <Project name = "Data Managers Inc" img = {require("../../img/image1.png")} creator = "test" points = {["Android based app coded using Java?", "Helps sales people at register identify items", "Users are able to their voice or text to look up their inventory"]}/> </SwiperSlide>
             <SwiperSlide> <Project name = "DMV Hours Tracker" img = {require("../../img/image7.png")} creator = "test" points = {["iOS based app coded using React-Native and Expo", "Helps students track driving hours for their license", "Utilizes Firebase backend to authenticate users and store user data"]}/> </SwiperSlide> 
     
